fc10bef934bb1ca0ecb54bfd682848ba0aa7ed05
张子恒 <44659470+zzhjs@users.noreply.github.com>
2019-12-09 10:59:00 +0800

    Add files via upload

---
+++ myjs.js
@@ -0,0 +1,89 @@
+/*
+* @Author: we
+* @Date:   2019-12-08 13:05:20
+* @Last Modified by:   we
+* @Last Modified time: 2019-12-09 10:48:16
+*/
+var box=document.getElementById("box");
+var slider=document.getElementById("slider");
+var navList=document.getElementById("nav").children;
+var right=document.getElementById("right");
+var left=document.getElementById("left");
+var p1=document.getElementById("p1");
+var i=1;
+navList[0].style.backgroundColor="red";
+navList[0].style.color="#ccc";
+function next(){
+	i++;
+		console.log(i);
+	navChange();
+	animate(slider,{left:-1200*i},function(){
+		if(slider.style.left=="-7200px"){
+			slider.style.left="-1200px";
+			i=1;
+		}
+	});
+}
+function pre(){
+	i--;
+	navChange();
+	animate(slider,{left:-1200*i},function(){
+		if(slider.style.left=="0px"){
+			slider.style.left="-6000px";
+			i=5;
+		}
+	});
+}
+var time1=setInterval(next,3000);
+box.onmouseover=function(){
+	animate(left,{opacity:50});
+	animate(right,{opacity:50});
+	clearInterval(time1);
+}
+box.onmouseout=function(){
+	animate(left,{opacity:0});
+	animate(right,{opacity:0});
+	time1=setInterval(next,3000);
+}
+right.onclick=next;
+left.onclick=pre;
+for(var j=0;j<navList.length;j++){
+	navList[j].i=j;
+	navList[j].onclick=function(){
+		i=this.i+1;
+		navChange();
+		animate(slider,{left:-1200*i});
+	}
+}
+function navChange(){
+	for(var k=0;k<navList.length;k++){
+		navList[k].style.backgroundColor="#ccc";
+		navList[k].style.color="black";
+	}
+	if(i===6){
+		navList[0].style.backgroundColor="red";
+		navList[0].style.color="#ccc";
+	}
+	else if(i===0){
+		navList[4].style.backgroundColor="red";
+		navList[4].style.color="#ccc";
+	}
+	else{		
+		navList[i-1].style.backgroundColor="red";
+		navList[i-1].style.color="#ccc";
+	}
+}
+
+setInterval(function(){
+
+			var sty=getComputedStyle(p1,null);
+			num=parseInt(sty.marginLeft);
+			if(num>-400){
+				num=num-1;
+				num=num+"px";
+				p1.style.marginLeft=num;
+			}		
+			else{
+				p1.style.marginLeft="1000px";
+			}	
+		}, 20)
